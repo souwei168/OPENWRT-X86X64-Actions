b05da75bdbf21cd18e82bba14c178a9849d48456
CNCTION <48990279+souwei168@users.noreply.github.com>
2021-11-15 17:26:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -66,4 +66,5 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus
 git clone --depth=1 https://github.com/vernesong/OpenClash
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
 git clone --depth=1 https://github.com/riverscn/openwrt-iptvhelper
+git clone --depth=1 https://github.com/hyy-666/luci-app-qBittorrent-enhanced
 popd
