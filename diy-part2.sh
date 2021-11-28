a37c2b26d0f649055b98b3067af1bd2ab61136a1
CNCTION <48990279+souwei168@users.noreply.github.com>
2021-11-28 14:53:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -70,5 +70,5 @@ git clone --depth=1 https://github.com/vernesong/OpenClash
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
 git clone --depth=1 https://github.com/riverscn/openwrt-iptvhelper
 #git clone --depth=1 https://github.com/hyy-666/luci-app-qBittorrent-enhanced
-git clone --depth=1 https://github.com/sirpdboy/luci-app-poweroffdevice
+#git clone --depth=1 https://github.com/sirpdboy/luci-app-poweroffdevice
 popd
